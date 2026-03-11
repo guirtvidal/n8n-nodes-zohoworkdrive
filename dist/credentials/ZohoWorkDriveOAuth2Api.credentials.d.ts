d769eaf4434cfea8d0f1f50f33bca2eab8f52184
guirtvidal <104011222+guirtvidal@users.noreply.github.com>
2026-03-11 18:26:27 +0000

    Changes

--- dist/credentials/ZohoWorkDriveOAuth2Api.credentials.d.ts
+++ dist/credentials/ZohoWorkDriveOAuth2Api.credentials.d.ts
@@ -1,7 +1,8 @@
 import { ICredentialType, INodeProperties } from 'n8n-workflow';
 export declare class ZohoWorkDriveOAuth2Api implements ICredentialType {
     name: string;
-    extends: string[];
     displayName: string;
+    extends: string[];
+    documentationUrl: string;
     properties: INodeProperties[];
 }
--- dist/credentials/ZohoWorkDriveOAuth2Api.credentials.js
+++ dist/credentials/ZohoWorkDriveOAuth2Api.credentials.js
@@ -4,8 +4,9 @@ exports.ZohoWorkDriveOAuth2Api = void 0;
 class ZohoWorkDriveOAuth2Api {
     constructor() {
         this.name = 'zohoWorkDriveOAuth2Api';
-        this.extends = ['oAuth2Api'];
         this.displayName = 'Zoho WorkDrive OAuth2 API';
+        this.extends = ['oAuth2Api'];
+        this.documentationUrl = 'https://workdrive.zoho.com/apidocs';
         this.properties = [
             {
                 displayName: 'Authorization URL',
@@ -20,11 +21,39 @@ class ZohoWorkDriveOAuth2Api {
                 default: 'https://accounts.zoho.com/oauth/v2/token',
             },
             {
+                displayName: 'Client ID',
+                name: 'clientId',
+                type: 'string',
+                default: '',
+            },
+            {
+                displayName: 'Client Secret',
+                name: 'clientSecret',
+                type: 'string',
+                typeOptions: {
+                    password: true,
+                },
+                default: '',
+            },
+            {
                 displayName: 'Scope',
                 name: 'scope',
-                type: 'hidden',
+                type: 'string',
                 default: 'WorkDrive.team.CREATE,WorkDrive.team.READ,WorkDrive.team.UPDATE,WorkDrive.team.DELETE,WorkDrive.teamfolders.CREATE,WorkDrive.teamfolders.READ,WorkDrive.teamfolders.UPDATE,WorkDrive.teamfolders.DELETE,WorkDrive.files.CREATE,WorkDrive.files.READ,WorkDrive.files.UPDATE,WorkDrive.files.DELETE',
-            }
+                description: 'Scopes for Zoho WorkDrive API',
+            },
+            {
+                displayName: 'Auth URI Query Parameters',
+                name: 'authQueryParameters',
+                type: 'string',
+                default: 'access_type=offline&prompt=consent',
+            },
+            {
+                displayName: 'Authentication',
+                name: 'authentication',
+                type: 'hidden',
+                default: 'body',
+            },
         ];
     }
 }
